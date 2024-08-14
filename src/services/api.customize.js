ae46740cfe57c5fcb2eaa545d1c1d63b94773855
duckAnh <leducanh.hna@gmail.com>
2024-08-14 12:00:39 +0700

    useEffect and customer url

--- src/services/api.customize.js
+++ src/services/api.customize.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_URL_DATABASE
     // timeout: 1000,
     // headers: { 'X-Custom-Header': 'foobar' }
 });
